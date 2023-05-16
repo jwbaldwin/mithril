e305be85dd2a07acf4d1faabedd1756a69b5759d
James Baldwin <jwbaldwin3@aol.com>
2023-05-16 11:29:51 -0400

    add spinner

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -89,6 +89,10 @@ export default function SignIn() {
                 type="submit"
                 className="group relative flex w-full justify-center rounded-md bg-gray-900 py-3 px-3 text-sm font-semibold text-white hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
               >
+                {params.isSubmitting ? <svg className="animate-spin absolute left-[25%] sm:left-[30%] mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                  <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                  <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                </svg> : null}
                 {params.isSubmitting ? <span>Signing in...</span> : <span>Sign in</span>}
               </button>
             </div>
