0ebd067ae897e291478a3bef95bddd743db06167
James Baldwin <jwbaldwin3@aol.com>
2023-06-20 14:59:47 -0400

    udpate middleware

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: [ /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - signin, register
-     */
-    '/((?!_next/static|_next/image|favicon.ico|signin|register|shopify|shopify/oauth|static|policies).*)',]
+  matcher: ["/dashboard", "/integrations", "/playground"]
 }
